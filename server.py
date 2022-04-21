d7d4ed77d07f69c8307d1e2dd9cd872885baeef0
Nathan <nathan.bourgoin01@gmail.com>
2022-04-21 16:09:07 -0400

    Correction exit

--- server.py
+++ server.py
@@ -13,36 +13,43 @@ if __name__ == "__main__":
     while True:
         client, address = server.accept()
         print(f"Connection Established - {address[0]}:{address[1]}")
+        client.settimeout(20.0)
+        try:
+            commande = client.recv(1024)
+            commande = commande.decode("utf-8")
+            commande = commande.upper()
 
-        commande = client.recv(1024)
-        commande = commande.decode("utf-8")
-        commande = commande.upper()
-
-        if commande == "TIME":
-            from datetime import datetime
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            message = current_time
-    
-        elif commande == "OS":
-            import platform
-            system = platform.system()
-            release = platform.release()
-            version = platform.version()
-
-            message = f"Systeme d'exploitation : {system}\n Release : {release}\n Version : {version}"
-
-        elif commande == "IP":
-            ipclient = address[0]
-            message = f"Votre adresse IP est : {ipclient}"
-
-        elif commande == "EXIT":
-            message = "Fermeture de la connexion..."
-            client.close()
+            if(not commande):
+                client.send(b"Timeout")
+                client.close()
+            
+            elif commande == "TIME":
+                from datetime import datetime
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                message = current_time
         
-        else :
-            message = "Cette commande n'est pas valide ou prise en charge par le serveur"
+            elif commande == "OS":
+                import platform
+                system = platform.system()
+                release = platform.release()
+                version = platform.version()
 
-        client.send(bytes(message, "utf-8"))
+                message = f"Systeme d'exploitation : {system}\n Release : {release}\n Version : {version}"
+
+            elif commande == "IP":
+                ipclient = address[0]
+                message = f"Votre adresse IP est : {ipclient}"
+
+            elif commande == "EXIT":
+                client.send(bytes("Fermeture de la connexion...", "utf-8"))
+                client.close()
+            
+            else :
+                message = "Cette commande n'est pas valide ou prise en charge par le serveur"
+
+            client.send(bytes(message, "utf-8"))
+        except socket.timeout:
+            print("TIMEOUT")
 
         
