14d12fbb803e42cb9fb388b328652271f8b5d60a
Nathan <nathan.bourgoin01@gmail.com>
2022-04-22 16:54:29 -0400

    ajustement calcul rgb

--- image.py
+++ image.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import matplotlib.pyplot as mp
 import numpy as np
 from tabulate import tabulate
@@ -17,29 +18,36 @@ def calculRGB(root):
             count = len(list)
             data = []
             #Si longueur de la liste plus grand que 2 = on a plus de 2 images
-            if count >= 1:
+            if count == 4:
                 #print(os.path.join(root, filename))
                 #Parcourir tous les fichiers de ce sous repertoire
+                first = True
+                tempRed = None
+                tempBlue = None
+                tempGreen = None
                 for img in list:
                     #Lire l'image et faire une moyenne de chaque valeur du RGB
                     image = mp.imread(f"{dir}/{img}")
-                    red=np.mean(image[:,:,0])
-                    green=np.mean(image[:,:,1])
-                    blue=np.mean(image[:,:,2])
-                    data.append([red,green,blue])   
-                #Lignes de test    
-                #print(tabulate(data,headers=['R','G','B'],tablefmt='fancy_grid'))
-                # print(np.sum(data,axis=0,dtype=np.int32)) 
-                #Calcul de la moyenne RGB de toutes les images du sous-dossier
-                dataSum = np.sum(data,axis=0,dtype=np.int32)
-                for i in range(3):
-                    dataSum[i] = dataSum[i] / count
-                #print(dataSum)
+                    red=image[:,:,0]
+                    green=image[:,:,1]
+                    blue=image[:,:,2]
+                    if(not first):
+                       tempRed = np.add(tempRed,red)
+                       tempGreen = np.add(tempGreen,red)
+                       tempBlue = np.add(tempBlue,red)
+                    else:
+                        tempRed = red
+                        tempBlue = blue
+                        tempGreen = green
+                red = np.around(tempRed / count)
+                blue = np.around(tempBlue / count)
+                green = np.around(tempGreen / count)
+
                 #Creation d'une image a partir d'un array
-                rgb = np.zeros((255, 255, 3), dtype=np.uint8)
-                rgb[..., 0] = (255-dataSum[0]) - np.arange(255)
-                rgb[..., 1] = dataSum[1]
-                rgb[..., 2] = (255-dataSum[2]) - np.arange(255)
+                rgb = np.zeros((250, 250, 3), dtype=np.uint8)
+                rgb[..., 0] = red
+                rgb[..., 1] = green
+                rgb[..., 2] = blue
                 mp.imsave(f'resultat/{filename}.png', rgb)
     print("--- %s seconds ---" % (time.time() - start_time))
 
